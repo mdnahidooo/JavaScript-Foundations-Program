e334cc98125dc4647d23112255c0105fd469bb7c
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-09-02 02:10:52 +0600

    3nd answer done

--- answer.js
+++ answer.js
@@ -40,3 +40,31 @@ function getDayType(day) {
 
 
 // console.log(getDayType("Tangail"));
+
+
+// Question No. 3
+
+function validateUsername(username) {
+    if (username.length < 4) {
+        return "Too Short";
+    }
+    else if (username.includes(" ")) {
+        return "No Space Allowed";
+    }
+    else if (username.toLowerCase().includes("admin")) {
+        return "Reserved Word";
+    }
+    else {
+        return "Available";
+    }
+}
+
+// console.log(validateUsername("nahid123"));
+// console.log(validateUsername("na"));
+// console.log(validateUsername("n a"));
+// console.log(validateUsername("nahid"));
+// console.log(validateUsername("nahid islam"));
+// console.log(validateUsername("superadmin99"));
+// console.log(validateUsername("Admin_Rahim"));
+
+
